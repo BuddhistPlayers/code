591c58feb092076fadd8e84b4f35d3bfccc0efcd
your father <you@example.com>
2018-09-26 14:41:33 +0800

    更新了数据标准化的读入

--- dataProcessing/data_normalize.py
+++ dataProcessing/data_normalize.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+#input
 df = pd.read_csv('C:\\Users\\DELL\\Desktop\\train1.csv',
                      #converters={column: json.loads for column in JSON_COLUMNS},
                      #dtype={'fullVisitorId': 'str'},
